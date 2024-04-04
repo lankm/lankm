a46b6da9b322f5945c59d4157ca4ef5dda35483f
Landon Moon <lankm@live.com>
2024-04-04 04:13:34 -0500

    A start but there has to be a better solution

--- sandbox/cpp/unit/unit.hpp
+++ sandbox/cpp/unit/unit.hpp
@@ -1,27 +1,45 @@
 #ifndef UNIT_GUARD_H
 #define UNIT_GUARD_H
 
-#include <cstdint>
-#include <iostream>
-#include <cmath>
-#include <iomanip>
+#define DEFINE_OPERATOR(class1, op, class2, class3) \
+class3 class1::operator op(const class2& other) {return this->val + other.val;}
+
+#define DEFINE_ASSIGNMENT_OPERATOR(class1, op, class2) \
+class1& class1::operator op(const class2& other) {this->val += other.val; return *this;}
+
+#define DEFINE_STANDARD_OPERATORS(class1) \
+DEFINE_ASSIGNMENT_OPERATOR(class1, +=, class1) \
+DEFINE_ASSIGNMENT_OPERATOR(class1, -=, class1) \
+DEFINE_ASSIGNMENT_OPERATOR(class1, *=, Unitless) \
+DEFINE_ASSIGNMENT_OPERATOR(class1, /=, Unitless) \
+DEFINE_OPERATOR(class1, +, class1, class1) \
+DEFINE_OPERATOR(class1, -, class1, class1) \
+DEFINE_OPERATOR(class1, *, Unitless, class1) \
+DEFINE_OPERATOR(class1, /, Unitless, class1)
+
+#define ADD_OPERATOR(op, class2, class3) \
+class3 operator op(const class2& other);
+
+#define ADD_ASSIGNMENT_OPERATOR(op, class2, class3) \
+class3& operator op(const class2& other);
+
+#define ADD_STANDARD_OPERATORS(class1) \
+class1() : Unit() {} \
+class1(double val) : Unit(val) {} \
+ADD_ASSIGNMENT_OPERATOR(+=, class1, class1) \
+ADD_ASSIGNMENT_OPERATOR(-=, class1, class1) \
+ADD_ASSIGNMENT_OPERATOR(*=, Unitless, class1) \
+ADD_ASSIGNMENT_OPERATOR(/=, Unitless, class1) \
+ADD_OPERATOR(+, class1, class1) \
+ADD_OPERATOR(-, class1, class1) \
+ADD_OPERATOR(*, Unitless, class1) \
+ADD_OPERATOR(/, Unitless, class1) \
+friend std::ostream& operator<<(std::ostream& os, const class1& obj) {return os << obj.val;}
 
 class Unit;
 class Unitless;
-class Mass;
 class Time;
-class Length;
-class Electric_Current;
-class Temperature;
-class Luminous_Intensity;
-class Area;
-class Volume;
 class Frequency;
-class Force;
-class Pressure;
-class Energy;
-class Power;
-class Electric_Charge;
 
 class Unit {
     public:
@@ -34,63 +52,40 @@ class Unit {
 /* s:0, m:0, kg:0, A:0, K:0, cd:0 */
 class Unitless : public Unit {
     public:
-        Unitless() : Unit() {}
-        Unitless(double val) : Unit(val) {}
+        ADD_STANDARD_OPERATORS(Unitless)
 
-        /* addition */
-        Unitless operator+(const Unitless& other);
-        Unitless& operator+=(const Unitless& other);
-        /* subtraction */
-        Unitless operator-(const Unitless& other);
-        Unitless& operator-=(const Unitless& other);
-        /* multiplication */
-        Unitless operator*(const Unitless& other);
-        Unitless& operator*=(const Unitless& other);
-        Time operator*(const Time& other);
-        /* division */
-        Unitless operator/(const Unitless& other);
-        Unitless& operator/=(const Unitless& other);
-
-        friend std::ostream& operator<<(std::ostream& os, const Unitless& obj) {return os << obj.val;}
+        ADD_OPERATOR(*, Time, Time)
+        ADD_OPERATOR(*, Frequency, Frequency)
 };
+
 /* s:1, m:0, kg:0, A:0, K:0, cd:0 */
 class Time : public Unit {
     public:
-        Time() : Unit() {}
-        Time(double val) : Unit(val) {}
+        ADD_STANDARD_OPERATORS(Time)
+        ADD_OPERATOR(/, Time, Unitless)
 
-        /* addition */
-        Time operator+(const Time& other);
-        Time& operator+=(const Time& other);
-        /* subtraction */
-        Time operator-(const Time& other);
-        Time& operator-=(const Time& other);
-        /* multiplication */
-        Time operator*(const Unitless& other);
-        Time& operator*=(const Unitless& other);
-        /* division */
-        Time operator/(const Unitless& other);
-        Time& operator/=(const Unitless& other);
+        ADD_OPERATOR(*, Frequency, Unitless)
 
-        friend std::ostream& operator<<(std::ostream& os, const Time& obj) {return os << obj.val;}
+};
+/* s:-1, m:0, kg:0, A:0, K:0, cd:0 */
+class Frequency : public Unit {
+    public:
+        ADD_STANDARD_OPERATORS(Frequency)
+        ADD_OPERATOR(/, Frequency, Unitless)
+
+        ADD_OPERATOR(*, Time, Unitless)
 };
 
-Unitless Unitless::operator+(const Unitless& other) {return this->val + other.val;}
-Unitless& Unitless::operator+=(const Unitless& other) {this->val += other.val; return *this;}
-Unitless Unitless::operator-(const Unitless& other) {return this->val - other.val;}
-Unitless& Unitless::operator-=(const Unitless& other) {this->val -= other.val; return *this;}
-Unitless Unitless::operator*(const Unitless& other) {return this->val * other.val;}
-Unitless& Unitless::operator*=(const Unitless& other) {this->val *= other.val; return *this;}
-Time Unitless::operator*(const Time& other) {return this->val * other.val;}
-Unitless Unitless::operator/(const Unitless& other) {return this->val / other.val;}
-Unitless& Unitless::operator/=(const Unitless& other) {this->val /= other.val; return *this;}
+DEFINE_STANDARD_OPERATORS(Unitless)
+DEFINE_OPERATOR(Unitless, *, Time, Time)
+DEFINE_OPERATOR(Unitless, *, Frequency, Frequency)
 
-Time Time::operator+(const Time& other) {return this->val + other.val;}
-Time& Time::operator+=(const Time& other) {this->val += other.val; return *this;}
-Time Time::operator-(const Time& other) {return this->val - other.val;}
-Time& Time::operator-=(const Time& other) {this->val -= other.val; return *this;}
-Time Time::operator*(const Unitless& other) {return this->val * other.val;}
-Time& Time::operator*=(const Unitless& other) {this->val *= other.val; return *this;}
-Time Time::operator/(const Unitless& other) {return this->val / other.val;}
-Time& Time::operator/=(const Unitless& other) {this->val /= other.val; return *this;}
+DEFINE_STANDARD_OPERATORS(Time)
+DEFINE_OPERATOR(Time, /, Time, Unitless)
+DEFINE_OPERATOR(Time, *, Frequency, Unitless)
+
+DEFINE_STANDARD_OPERATORS(Frequency)
+DEFINE_OPERATOR(Frequency, /, Frequency, Unitless)
+DEFINE_OPERATOR(Frequency, *, Time, Unitless)
+
 #endif
