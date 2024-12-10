c2ebce362244ca08f86345e7b5be27ab7b70bdfe
Landon Moon <lankm@live.com>
2024-12-09 21:30:48 -0600

    got the html helper working

--- sandbox/web/warhammer/lib/page.php
+++ sandbox/web/warhammer/lib/page.php
@@ -1,5 +1,6 @@
 <?php
 include("tags.php");
+include("constants.php");
 
 class AbstractPage {
     private string $lang;
@@ -15,33 +16,19 @@ class AbstractPage {
         return DOCTYPE_('html').
         html_()(
             head_()(
-                meta_(charset: 'UTF-8').
-                link_(href: '/account/index.css', rel: 'stylesheet').
-                title_()("Hello, World")
-            ).
+                meta_(charset:"UTF-8"),
+                title_()("WH40k Helper"),
+            ),
             body_()(
-                header_(class: 'test')(
-                    "Header"
-                ).
-                main_()(
-                    h1_()("List").
-                    ul_()(
-                        li_()(
-                            "1"
-                        ).
-                        li_()(
-                            sup_()("2")
-                        ).
-                        li_()(
-                            "3"
-                        )
-                    )
-                ).
-                footer_(class: "test")(
-                    "footer"
-                ).
-                script_(src: '/account/index.js')()
-            )
+                p_()(HTTP_STATUS::OK),
+                input_(value: "\"><input value=checked", type: "text>")()
+            ),
         );
+    }
+}
+
+class HomePage extends AbstractPage {
+    public function __construct() {
+        $this->lang = 'en';
     }
 }
--- sandbox/web/warhammer/lib/tags.php
+++ sandbox/web/warhammer/lib/tags.php
@@ -1,155 +1,182 @@
 <?php
-class HtmlTag {
-    private static function serializeAttributes(array $attr) {
-        $result = '';
-        foreach($attr as $key => $value) {
+class SafeHtmlElement {
+    public function __construct(
+        private string $name,
+        private string $attributes='',
+        private string $content='',
+    ) {}
+    public function __toString(): string {
+        extract(get_object_vars($this));
+        if(empty($content))
+            return "<$name$attributes>";
+        return "<$name$attributes>$content</$name>";
+    }
+}
+
+class HtmlElement {
+    public function __construct(
+        private string $name,
+        private array $attributes
+    ) {}
+
+    private function parseAttributes(array $attributes) {
+        $parsedAttributes = '';
+        foreach($attributes as $key => $value) {
             $encoded = htmlspecialchars($value);
-            $result .= match (is_int($key)) {
+            $parsedAttributes .= match (is_int($key)) {
                 true => " $encoded",
-                false => " $key=$encoded"
+                false => " $key=\"$encoded\""
             };
         }
-        return $result;
+        return $parsedAttributes;
     }
-    public static function wrap(string $name, array $attr) {
-        $attr = self::serializeAttributes($attr);
-        return function($content='') use ($name, $attr) {
-            return "<$name$attr>$content</$name>";
-        };
-    }
-    public static function void(string $name, array $attr) {
-        $attr = self::serializeAttributes($attr);
-        return "<$name$attr/>";
-    }
-    public static function tag(string $name, array $attr) {
-        $attr = self::serializeAttributes($attr);
-        return "<$name$attr>";
-    }
-}
-
-$TAGS = [
-    '!DOCTYPE'  => 'tag',
-    'a'         => 'wrap',
-    'abbr'      => 'wrap',
-    'address'   => 'wrap',
-    'area'      => 'wrap',
-    'article'   => 'wrap',
-    'aside'     => 'wrap',
-    'audio'     => 'wrap',
-    'b'         => 'wrap',
-    'base'      => 'wrap',
-    'bdi'       => 'wrap',
-    'bdo'       => 'wrap',
-    'blockquote'=> 'wrap',
-    'body'      => 'wrap',
-    'br'        => 'wrap',
-    'button'    => 'wrap',
-    'canvas'    => 'wrap',
-    'caption'   => 'wrap',
-    'cite'      => 'wrap',
-    'code'      => 'wrap',
-    'col'       => 'wrap',
-    'colgroup'  => 'wrap',
-    'data'      => 'wrap',
-    'datalist'  => 'wrap',
-    'dd'        => 'wrap',
-    'del'       => 'wrap',
-    'details'   => 'wrap',
-    'dfn'       => 'wrap',
-    'dialog'    => 'wrap',
-    'div'       => 'wrap',
-    'dl'        => 'wrap',
-    'dt'        => 'wrap',
-    'em'        => 'wrap',
-    'embed'     => 'wrap',
-    'fieldset'  => 'wrap',
-    'figcaption'=> 'wrap',
-    'figure'    => 'wrap',
-    'footer'    => 'wrap',
-    'form'      => 'wrap',
-    'h1'        => 'wrap',
-    'h2'        => 'wrap',
-    'h3'        => 'wrap',
-    'h4'        => 'wrap',
-    'h5'        => 'wrap',
-    'h6'        => 'wrap',
-    'button'    => 'wrap',
-    'head'      => 'wrap',
-    'header'    => 'wrap',
-    'hgroup'    => 'wrap',
-    'hr'        => 'wrap',
-    'html'      => 'wrap',
-    'i'         => 'wrap',
-    'iframe'    => 'wrap',
-    'img'       => 'wrap',
-    'input'     => 'wrap',
-    'ins'       => 'wrap',
-    'kbd'       => 'wrap',
-    'label'     => 'wrap',
-    'legend'    => 'wrap',
-    'li'        => 'wrap',
-    'link'      => 'tag',
-    'main'      => 'wrap',
-    'map'       => 'wrap',
-    'mark'      => 'wrap',
-    'menu'      => 'wrap',
-    'meta'      => 'tag',
-    'meter'     => 'wrap',
-    'nav'       => 'wrap',
-    'noscript'  => 'wrap',
-    'object'    => 'wrap',
-    'ol'        => 'wrap',
-    'optgroup'  => 'wrap',
-    'option'    => 'wrap',
-    'output'    => 'wrap',
-    'p'         => 'wrap',
-    'param'     => 'wrap',
-    'picture'   => 'wrap',
-    'pre'       => 'wrap',
-    'progress'  => 'wrap',
-    'q'         => 'wrap',
-    'rp'        => 'wrap',
-    'rt'        => 'wrap',
-    'ruby'      => 'wrap',
-    's'         => 'wrap',
-    'samp'      => 'wrap',
-    'script'    => 'wrap',
-    'search'    => 'wrap',
-    'section'   => 'wrap',
-    'select'    => 'wrap',
-    'small'     => 'wrap',
-    'source'    => 'wrap',
-    'span'      => 'wrap',
-    'strong'    => 'wrap',
-    'style'     => 'wrap',
-    'sub'       => 'wrap',
-    'summary'   => 'wrap',
-    'sup'       => 'wrap',
-    'svg'       => 'wrap',
-    'table'     => 'wrap',
-    'tbody'     => 'wrap',
-    'td'        => 'wrap',
-    'template'  => 'wrap',
-    'textarea'  => 'wrap',
-    'tfoot'     => 'wrap',
-    'th'        => 'wrap',
-    'thead'     => 'wrap',
-    'time'      => 'wrap',
-    'title'     => 'wrap',
-    'tr'        => 'wrap',
-    'track'     => 'wrap',
-    'u'         => 'wrap',
-    'ul'        => 'wrap',
-    'var'       => 'wrap',
-    'video'     => 'wrap',
-    'wbr'       => 'wrap',
-];
-
-foreach($TAGS as $tag => $type) {
-    $function_name = preg_replace('/^[^a-zA-Z]+/','',$tag)."_";
-    eval("
-        function $function_name(...\$attr) {
-            return HtmlTag::$type(\"$tag\", \$attr);
+    private function parseContent(array $content) {
+        $parsedContent = '';
+        foreach($content as $value) {
+            $parsedContent .= match (is_string($value)) {
+                true => htmlspecialchars($value),
+                false => $value
+            };
         }
-    ");
+        return $parsedContent;
+    }
+
+    public function __toString(): string {
+        $parsedAttributes = $this->parseAttributes($this->attributes);
+        return new SafeHtmlElement($this->name, $parsedAttributes);
+    }
+    public function __invoke(...$content): SafeHtmlElement {
+        $parsedAttributes = $this->parseAttributes($this->attributes);
+        $parsedContent = $this->parseContent($content);
+        return new SafeHtmlElement($this->name, $parsedAttributes, $parsedContent);
+    }
+
+    
+    private const array TAGS = [
+        "!DOCTYPE" => "DOCTYPE_",
+        "a"         => "a_",
+        "abbr"      => "abbr_",
+        "address"   => "address_",
+        "area"      => "area_",
+        "article"   => "article_",
+        "aside"     => "aside_",
+        "audio"     => "audio_",
+        "b"         => "b_",
+        "base"      => "base_",
+        "bdi"       => "bdi_",
+        "bdo"       => "bdo_",
+        "blockquote"=> "blockquote_",
+        "body"      => "body_",
+        "br"        => "br_",
+        "button"    => "button_",
+        "canvas"    => "canvas_",
+        "caption"   => "caption_",
+        "cite"      => "cite_",
+        "code"      => "code_",
+        "col"       => "col_",
+        "colgroup"  => "colgroup_",
+        "data"      => "data_",
+        "datalist"  => "datalist_",
+        "dd"        => "dd_",
+        "del"       => "del_",
+        "details"   => "details_",
+        "dfn"       => "dfn_",
+        "dialog"    => "dialog_",
+        "div"       => "div_",
+        "dl"        => "dl_",
+        "dt"        => "dt_",
+        "em"        => "em_",
+        "embed"     => "embed_",
+        "fieldset"  => "fieldset_",
+        "figcaption"=> "figcaption_",
+        "figure"    => "figure_",
+        "footer"    => "footer_",
+        "form"      => "form_",
+        "h1"        => "h1_",
+        "h2"        => "h2_",
+        "h3"        => "h3_",
+        "h4"        => "h4_",
+        "h5"        => "h5_",
+        "h6"        => "h6_",
+        "head"      => "head_",
+        "header"    => "header_",
+        "hgroup"    => "hgroup_",
+        "hr"        => "hr_",
+        "html"      => "html_",
+        "i"         => "i_",
+        "iframe"    => "iframe_",
+        "img"       => "img_",
+        "input"     => "input_",
+        "ins"       => "ins_",
+        "kbd"       => "kbd_",
+        "label"     => "label_",
+        "legend"    => "legend_",
+        "li"        => "li_",
+        "link"      => "link_",
+        "main"      => "main_",
+        "map"       => "map_",
+        "mark"      => "mark_",
+        "menu"      => "menu_",
+        "meta"      => "meta_",
+        "meter"     => "meter_",
+        "nav"       => "nav_",
+        "noscript"  => "noscript_",
+        "object"    => "object_",
+        "ol"        => "ol_",
+        "optgroup"  => "optgroup_",
+        "option"    => "option_",
+        "p"         => "p_",
+        "param"     => "param_",
+        "picture"   => "picture_",
+        "pre"       => "pre_",
+        "progress"  => "progress_",
+        "q"         => "q_",
+        "rp"        => "rp_",
+        "rt"        => "rt_",
+        "ruby"      => "ruby_",
+        "s"         => "s_",
+        "samp"      => "samp_",
+        "script"    => "script_",
+        "search"    => "search_",
+        "section"   => "section_",
+        "select"    => "select_",
+        "small"     => "small_",
+        "source"    => "source_",
+        "span"      => "span_",
+        "strong"    => "strong_",
+        "style"     => "style_",
+        "sub"       => "sub_",
+        "summary"   => "summary_",
+        "sup"       => "sup_",
+        "svg"       => "svg_",
+        "table"     => "table_",
+        "tbody"     => "tbody_",
+        "td"        => "td_",
+        "template"  => "template_",
+        "textarea"  => "textarea_",
+        "tfoot"     => "tfoot_",
+        "th"        => "th_",
+        "thead"     => "thead_",
+        "time"      => "time_",
+        "title"     => "title_",
+        "tr"        => "tr_",
+        "track"     => "track_",
+        "u"         => "u_",
+        "ul"        => "ul_",
+        "var"       => "var_",
+        "video"     => "video_",
+        "wbr"       => "wbr_",
+    ];
+
+    public static function loadTags(): void {
+        //adding functions to global namespace
+        foreach(self::TAGS as $tag => $fn_name) {
+            eval("
+                function $fn_name(...\$attr) {
+                    return new HtmlElement(\"$tag\", \$attr);
+                }
+            ");
+        }
+    }
 }
+HtmlElement::loadTags();
